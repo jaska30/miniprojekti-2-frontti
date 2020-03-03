01f2bc9996c358038ce3b68bbf52910f6c20752d
Daniel <d_boije86@hotmail.com>
2020-03-03 15:33:29 +0200

    new game, game boardin aloitus

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 class GameBoard extends Component {
+    
     render() {
         return (
             <div>
-                
+                <p>Hello</p>
+                <p>{this.props.user}</p>
+                <p>Score</p>
+                <input value="New Game" type="button" onClick={this.props.newgame}/>
             </div>
         );
     }
