7f0e8cc29e71502674876e24e1b38a4fc0eace3f
nanaym <59910434+nanaym@users.noreply.github.com>
2020-03-03 18:09:52 +0200

    databasen kuvat näkyy

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { getPics } from '../service';
+import Cards from "./Cards"
 
 class GameBoard extends Component {
+    state = { images: [], msg: "Haetaan kuvia" }
+    componentDidMount() {
+        this.picList();
+    }
+    picList = () => {
+        getPics()
+            .then(res => {
+                console.log(res.data)
+                this.setState({ images: res.data })
+            });
+    }
     render() {
         return (
             <div>
-                
+                <Cards info={this.state.images}/>
             </div>
         );
     }
