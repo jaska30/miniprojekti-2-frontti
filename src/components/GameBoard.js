a21a0247d9f4a9973fd6d7a607a0347ec91929b6
jaakko koskela <jaska.koskela@gmail.com>
2020-03-04 16:08:45 +0200

    kuvien sekoitus pelitauluun toteutettu

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -14,124 +14,34 @@ class GameBoard extends Component {
     //             this.setState({ images: res.data })
     //         });
     // }
+    state = { images: [{nimi:"bear.png"}, {nimi:'bunny.png'}, {nimi:'cat.png'}, {nimi:'deer.png'}, {nimi:'dog.png'}, {nimi:'duck.png'}, {nimi:'eagle.png'}, {nimi:'giraffe.png'}, {nimi:'hedgehog.png'}, {nimi:'kangaroo.png'}, {nimi:'lion.png'}, {nimi:'llama.png'}, {nimi:'mouse.png'}, {nimi:'owl.png'}, {nimi:'piggy.png'}, {nimi:'puppy.png'}, {nimi:'snake.png'}, {nimi:'squirrel.png'},
+    {nimi:"bear.png"}, {nimi:'bunny.png'}, {nimi:'cat.png'}, {nimi:'deer.png'}, {nimi:'dog.png'}, {nimi:'duck.png'}, {nimi:'eagle.png'}, {nimi:'giraffe.png'}, {nimi:'hedgehog.png'}, {nimi:'kangaroo.png'}, {nimi:'lion.png'}, {nimi:'llama.png'}, {nimi:'mouse.png'}, {nimi:'owl.png'}, {nimi:'piggy.png'}, {nimi:'puppy.png'}, {nimi:'snake.png'}, {nimi:'squirrel.png'}]}
+
+    componentDidMount() {
+        this.shuffleImages();
+    }
+
+     shuffleImages = () => {
+         var shuffled = this.state.images.sort((a, b) => .5- Math.random());
+         this.setState({images: shuffled })
+         console.log(this.state.images);
+    }
+        
+    
+
     render() {
+        var nodes = 
+         this.state.images.map(function(card) {
+            return ( <Cards  card={card}/>
+                )})
         return (
             
             <div className="gameboard">
                 <div className="images">
-                <div>
-                    <img src={require('../pics/bear.png')} style={{width:'100px', height:'100px'}} />
-                </div>
-                <div>
-                    <img src={require('../pics/bear.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/bunny.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/bunny.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/cat.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/cat.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                
-                <div>
-                    <img src={require('../pics/deer.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/deer.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/dog.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/dog.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/duck.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/duck.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/eagle.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/eagle.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/giraffe.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/giraffe.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/hedgehog.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/hedgehog.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/kangaroo.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/kangaroo.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/lion.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/lion.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/llama.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/llama.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/mouse.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/mouse.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/owl.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/owl.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/piggy.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/piggy.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/puppy.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/puppy.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/snake.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/snake.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/squirrel.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <div>
-                    <img src={require('../pics/squirrel.png')} style={{width:'100px', height:'100px'}}/>
-                </div>
-                <p>{this.props.user}</p>
-                <p>Score</p>
-                <input value="New Game" type="button" onClick={this.props.newgame} />
+                    {nodes}
             </div>
+            <p>Score</p>
+                <input value="New Game" type="button" onClick={this.props.newgame}/>
             </div>
         );
     }
