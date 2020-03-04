1ab207c06ed28ac24d66326f1c7e769f8422e2c6
jaakko koskela <jaska.koskela@gmail.com>
2020-03-04 11:07:37 +0200

    kuvat lisätty kansioon ja niistä tehty grid

--- src/components/User.js
+++ src/components/User.js
@@ -4,7 +4,7 @@ export default class User extends Component {
     render() {
         
         return (
-                <div className="scoretable" style={{width:"100px", border:"solid"}}>
+                <div className="scoretable">
                     <p className="username">{this.props.user.username}, {this.props.user.score}</p>
                 </div>
         )
